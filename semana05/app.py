a457b2dd073695a032ba9b7765f8c5ac5a10eb36
Kevin Ramos <ljkevin14@gmail.com>
2026-09-17 22:55:22 -0500

    feat: Conectando python con bd

--- semana05/app.py
+++ semana05/app.py
@@ -1,6 +1,12 @@
 # from LIBRERIA import CLASES, FUNCIONES, que queremos usar de la libreria.
 #request nos dara toda la informacion proveniente del cliente, y solo puede ser llamador dentro de un controlador
 from flask import Flask, request
+from werkzeug.exceptions import UnsupportedMediaType
+from psycopg import connect
+
+# postgresql://NOMBRE_USUARIO:PASSWORD_USUARIO@HOST:PUERTO/NOMBRE_BD
+credenciales = "postgresql://postgres:123456 @127.0.0.1:5432/flask_db"
+conexion = connect(conninfo=credenciales)
 
 # __name__ > Variable global de python que sirve para indicar si el archivo en el cual nos encontramos se esta ejecutando directamente o no en la terminal 
 # python app.py > el valor de esta variable sera __main__
@@ -34,15 +40,57 @@ def gestionar_productos():
     print(request.method)
 
     if request.method == 'GET':
+        # El encargado de iniciar la conexion con la BD
+        cursor = conexion.cursor()
+
+        #Ejecuramos el comando en la bd
+        cursor.execute("SELECT * FROM productos")
+
+        # Para obtener el resultado (si es necesario) usamos los metodos fetchone, fetchall, fetchmany
+        productos_bd = cursor.fetchall()
+
+        print(productos_bd)
+
+        resultado = []
+        for producto in productos_bd:
+            resultado.append({
+                "id": producto[0],
+                "nombre": producto[1],
+                "precio": float(producto[2]),
+                "cantidad": producto[3]
+            })
+
         # Los controladores (es la logica del endpoint) suelen retornar diccionarios que estos seran interpretados en JSON o tambien se suele retornar listas (arreglos)
         return {
             "message": "Los productos son:",
-            "content": productos
+            "content": resultado
+
         }
     elif request.method == 'POST':
-        return {
-            "message": "Producto creado exitosamente"
-        }
+        #se usa para crear nueva informacion proveniente del frontend
+        # request.get_data retorna la informacion del cliente como si fuera un string
+        # request.get_json retorna la infiramcion del cliente como si fuera un diccionario para que python pueda entender
+        #print(request.get_data())
+        #print(request.get_json())
+
+        #handler, manejador de errores
+        #Le colocamos un try ya que como no hay danos no esta trayendo nada y nos mandara error
+        try:
+            data = request.get_json()
+            #Ahora con la informacion correcta agregamos este producto a nuestra lista
+            productos.append(data)
+            return {
+                "message": "Producto creado exitosamente"
+            }
+        except UnsupportedMediaType:
+        #except Exception as error:
+            #Exceiption es la clase primordial de los errores para poder saber que error es alias es as
+            #print(type(error)) #para saber el error cual es
+            return {
+                "message": "Se debe enviar la informacion en formato json"
+            }
+
+        
 
 
 
